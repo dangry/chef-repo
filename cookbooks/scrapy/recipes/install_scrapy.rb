d834cad8801a474af40fda098e22a51c105db778
Gonzalo Rodríguez <g_rod@live.cl>
2016-05-06 14:07:19 -0700

    Update install_scrapy.rb

--- cookbooks/scrapy/recipes/install_scrapy.rb
+++ cookbooks/scrapy/recipes/install_scrapy.rb
@@ -5,3 +5,8 @@ end
 execute "apt-get install libxml2-dev libxslt1-dev python-dev" do
   command "apt-get install libxml2-dev libxslt1-dev python-dev"
 end
+
+execute "apt-get install libxml2-dev libxslt1-dev python-dev" do
+  command "cd /opt/"
+  command "scrapy startproject djCrawler"
+end
