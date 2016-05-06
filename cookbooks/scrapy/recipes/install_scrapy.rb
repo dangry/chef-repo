fd402787537f7e9779e462b59b3fa6bae541984e
Gonzalo Rodríguez <g_rod@live.cl>
2016-05-06 13:17:49 -0700

    Update install_scrapy.rb

--- cookbooks/scrapy/recipes/install_scrapy.rb
+++ cookbooks/scrapy/recipes/install_scrapy.rb
@@ -2,10 +2,6 @@ execute "pip install scrapy" do
   command "pip install scrapy"
 end
 
-execute "pip install libffi-dev" do
-  command "pip install libffi-dev"
-end
-
 execute "pip install cryptography" do
   command "pip install cryptography"
 end
