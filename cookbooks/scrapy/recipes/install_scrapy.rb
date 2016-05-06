c46cae21d44a81749188082bf6e40ec0959d0809
Gonzalo Rodríguez <g_rod@live.cl>
2016-05-06 13:24:20 -0700

    Update install_scrapy.rb

--- cookbooks/scrapy/recipes/install_scrapy.rb
+++ cookbooks/scrapy/recipes/install_scrapy.rb
@@ -2,6 +2,6 @@ execute "pip install scrapy" do
   command "pip install scrapy"
 end
 
-execute "pip install cryptography" do
-  command "pip install cryptography"
+execute "apt-get install libxml2-dev libxslt1-dev python-dev" do
+  command "apt-get install libxml2-dev libxslt1-dev python-dev"
 end
